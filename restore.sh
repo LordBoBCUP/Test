86fa21b72792172853f0611c64e05df9caee2780
Alex <lordbobcup@gmail.com>
2019-06-14 17:04:04 +1200

    Delete restore.sh

--- restore.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-mkdir /tmp/mx
-# Get Chainspec
-curl https://raw.githubusercontent.com/LordBoBCUP/Test/master/chainspec.json --output /tmp/mx/chainspec.json
-curl https://raw.githubusercontent.com/LordBoBCUP/Test/master/backup.bin --output /tmp/mx/chainspec.json
-
-# Run Backup
-    /usr/local/bin/substrate import-blocks -d /tmp/mx --chain /tmp/mx/chainspec.json /tmp/mx/backup.bin
